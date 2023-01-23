b0db5f28a21473ffcb5f4e064529c9c49dee09da
Elsayed Kamal <elsayedkamal581999@gmail.com>
2023-01-23 13:29:21 +0200

    “wip”

--- app/Collectors/Scrapers/Direct/Faselhd.php
+++ app/Collectors/Scrapers/Direct/Faselhd.php
@@ -2,9 +2,10 @@
 
 namespace App\Collectors\Scrapers\Direct;
 
-use App\Services\Helpers\Request;
 use App\Collectors\Helpers\JaroWinkler;
+use App\Services\Helpers\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
@@ -81,39 +82,56 @@ class Faselhd
         $client = new_http_client();
         $crawler = new HttpBrowser($client);
         if ($type == "tv") {
-            // $seasonId = $crawler->request('GET', $show['href'])->filter("#seasonList .seasonDiv")->eq($season - 1)->attr('data-href');
-            // $data = Http::withHeaders([
-            //     "x-requested-with" => "XMLHttpRequest",
-            //     "referer" => self::DOMAIN
-            // ])->asForm()->post(self::DOMAIN . "/series-ajax/?_action=get_season_list&_post_id=" . $seasonId, [
-            //     "seasonID" => $seasonId,
-            // ])->body();
-            // $crawler_html = new Crawler();
-            // $crawler_html->addHTMLContent($data);
+            $seasonId = $crawler->request('GET', $show['href'])->filter("#seasonList .seasonDiv")->eq($season - 1)->attr('data-href');
+            $data = Http::withHeaders([
+                "x-requested-with" => "XMLHttpRequest",
+                "referer" => self::DOMAIN
+            ])->asForm()->post(self::DOMAIN . "/series-ajax/?_action=get_season_list&_post_id=" . $seasonId, [
+                "seasonID" => $seasonId,
+            ])->body();
+            $crawler_html = new Crawler();
+            $crawler_html->addHTMLContent($data);
 
-            // $episodes = $crawler_html->filter("#epAll a")->each(function ($el) {
-            //     return $el->attr('href');
-            // });
+            // $episodes = $crawler_html->filter("#epAll")->html();
+            $episodes = $crawler_html->filter("#epAll a")->each(function ($el) {
+                return $el->attr('href');
+            });
+
+            $show['href'] = $episodes[$episode - 1] ?? null;
+
+            // // select all links between href "link" using regx
+            // $re = '~href="([^"]+)"~';
+            // preg_match_all($re, $episodes, $matches, PREG_SET_ORDER, 0);
+            // $episodes = collect($matches)->map(function ($el) {
+            //     return $el[1];
+            // })->toArray();
+
+
+            // dd($episodes);
+
+            // https://www.faselhd.club/episodes/%d9%85%d8%b3%d9%84%d8%b3%d9%84-mayor-kingstown-%d8%a7%d9%84%d9%85%d9%88%d8%b3%d9%85-%d8%a7%d9%84%d8%a7%d9%88%d9%84-%d8%a7%d9%8
+
+            // https://www.faselhd.club/episodes/%d9%85%d8%b3%d9%84%d8%b3%d9%84-mayor-kingstown-%d8%a7%d9%84%d9%85%d9%88%d8%b3%d9%85-%d8%a7%d9%84%d8%ab%d8%a7%d9%86%d9%8a-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-1
+
             // $show['href'] = $episodes[$episode - 1] ?? null;
 
-            $show_name = parse_url($show['href'], PHP_URL_PATH);
-            $show_name = str_replace("/seasons/%d9%85%d8%b3%d9%84%d8%b3%d9%84-", "", $show_name);
-            $show_name = str_replace("/seasons/series-", "", $show_name);
-            $season_arabic = seasonNumberAsTextInArabic($season);
-            $show['href'] = "https://www.faselhd.club/episodes/مسلسل-$show_name-الموسم-$season_arabic-الحلقة-$episode";
+            // $show_name = parse_url($show['href'], PHP_URL_PATH);
+            // $show_name = str_replace("/seasons/%d9%85%d8%b3%d9%84%d8%b3%d9%84-", "", $show_name);
+            // $show_name = str_replace("/seasons/series-", "", $show_name);
+            // $season_arabic = seasonNumberAsTextInArabic($season);
+            // $show['href'] = "https://www.faselhd.club/episodes/مسلسل-$show_name-الموسم-$season_arabic-الحلقة-$episode";
         }
 
         if (is_null($show['href'])) return null;
 
-        // if ($type == "tv" && $episode == 1) {
-        //     $src = $crawler_html->filter('iframe[name="player_iframe"]')->attr('src');
-        // } else {
-        //     $content = $crawler->request('GET', $show['href']);
-        //     $src = $content->filter('iframe[name="player_iframe"]')->attr('src');
-        // }
-
         $content = $crawler->request('GET', $show['href']);
-        $src = $content->filter('iframe[name="player_iframe"]')->attr('src');
+        $src = $content->filter('iframe[name="player_iframe"]')->attr('src') ?? null;
+
+        if(is_null($src)) return null;
+
+        // dd($show['href']);
+        // $content = $crawler->request('GET', $show['href']);
+        // $src = $content->filter('iframe[name="player_iframe"]')->attr('src');
 
         $content = $crawler->request('GET', $src);
         $script = $content->filter("script")->eq(0)->text();
